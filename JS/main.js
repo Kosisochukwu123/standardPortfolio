b012555c28b3dcfe9115ea6c0d99f896740c853d
kosi <kosiogbunuko@gmail.com>
2025-09-07 19:52:21 +0100

    new version

--- JS/main.js
+++ JS/main.js
@@ -1,27 +1,17 @@
 // Dark MOde Feature code-----------------------
 const darkMode = document.getElementById("dark-mode");
 
-// function setDarkMode(isDarkMode) {
-//   if (isDarkMode) {
-//     document.body.classList.add("blackScreen");
-//     sessionStorage.setItem("darkMode", "enabled");
-//   } else {
-//     document.body.classList.remove("blackScreen");
-//     sessionStorage.setItem("darkMode", "disabled");
-//   }
-// }
-
 darkMode.addEventListener("click", () => {
   const isDarkMode = document.body.classList.toggle("blackScreen");
-  //   setDarkMode(isDarkMode);
+    sessionStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
 });
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const darkMode = sessionStorageStorage.getitem('darkMode');
-//     if (darkMode === 'enabled') {
-//         document.body.classList.add('blackScreen');
-//     }
-// })
+document.addEventListener('DOMContentLoaded', () => {
+    const darkMode = sessionStorage.getItem('darkMode');
+    if (darkMode === 'enabled') {
+        document.body.classList.add('blackScreen');
+    }
+})
 
 // BACK TO TOP FEATURE CODE
 
