8a8040271c5013a48ea3d5cba7dc619ae316df92
kosi <kosiogbunuko@gmail.com>
2025-09-08 13:33:06 +0100

    one today

--- JS/main.js
+++ JS/main.js
@@ -3,18 +3,17 @@ const darkMode = document.getElementById("dark-mode");
 
 darkMode.addEventListener("click", () => {
   const isDarkMode = document.body.classList.toggle("blackScreen");
-    sessionStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
+  sessionStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const darkMode = sessionStorage.getItem('darkMode');
-    if (darkMode === 'enabled') {
-        document.body.classList.add('blackScreen');
-    }
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const darkMode = sessionStorage.getItem("darkMode");
+  if (darkMode === "enabled") {
+    document.body.classList.add("blackScreen");
+  }
+});
 
 // BACK TO TOP FEATURE CODE
-
 const backToTop = document.getElementById("back-to-top");
 
 window.addEventListener("scroll", () => {
@@ -28,3 +27,28 @@ window.addEventListener("scroll", () => {
 backToTop.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+// -------------------for disabled links in the "my recent projcts" section-----------------------
+const disableLink = document.getElementById("myLink");
+let isEnabled = false;
+
+disableLink.addEventListener("click", function (event) {
+  if (!isEnabled) {
+    event.preventDefault();
+  }
+});
+
+// -----------funtion that delays the button on the header to delay before moving to the next html file so the animation on it can show-----
+function goToNextPage(url) {
+  setTimeout(() => {
+    window.location.href = url;
+  }, 1000);
+}
+
+
+// -------------------------------
+
+
+let azu = "me"
+
+console.log(typeof azu);
